ae22403524b92205ea81c39d74e5f01777340ea8
JasonB2075 <bokagne.jason@institutsaintjean.org>
2025-04-02 21:12:51 +0200

    corrections de bugs mineurs

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -3,8 +3,8 @@ import { test, expect } from '@playwright/test';
 test('Ajouter un produit au panier', async ({ page }) => {
     await page.goto('https://ztrain-web.vercel.app/home', {waitUntil: 'domcontentloaded' });
     await page.locator('.style_card_body__QuFGN').first().click();
-    const addToCartButton = page.locator('button:has-text("Ajouter au panier")');
-    await addToCartButton.click();
+    const addButton = page.locator('button:has-text("Ajouter au panier")');
+    await addButton.click();
     const notification = page.locator('.ant-notification-notice-message');
     await expect(notification).toHaveText('Ajout produit au panier');
     const cartCount = page.locator('#style_content_cart_wrapper__mqNbf');
